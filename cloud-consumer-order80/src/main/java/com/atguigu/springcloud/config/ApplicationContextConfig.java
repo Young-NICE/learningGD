ba186f366b05304f1fe3e18d1f640d331ef24059
YANGPEIPEI <137204307@qq.com>
2020-06-01 01:30:41 +0200

    add payment8002

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +16,7 @@ public class ApplicationContextConfig {
     * 相当于 applicationContext.xml <bean id="" class="">
     * */
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
