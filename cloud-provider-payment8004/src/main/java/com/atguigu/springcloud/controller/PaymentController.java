9fe1e43845bddc5ca6336f8d21093e61ff72972f
YANGPEIPEI <137204307@qq.com>
2020-06-22 17:11:02 +0200

    add zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author: 13720
+ * @date: 2020/6/10
+ * @description:
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk(){
+        return "springcloud with zookeeper: "+serverPort+"\t"+
+                UUID.randomUUID().toString();
+    }
+}
