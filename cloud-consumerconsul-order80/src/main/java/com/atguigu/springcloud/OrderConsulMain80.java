d22064851e60241aef6c0f7723f8de000e71cc40
YANGPEIPEI <137204307@qq.com>
2020-06-24 23:59:20 +0200

    增加了consul部分

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: 13720
+ * @date: 2020/6/25
+ * @description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
