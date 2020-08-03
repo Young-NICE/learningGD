88407f24a06c6414c08343cd99daaf3cf9599d15
YANGPEIPEI <137204307@qq.com>
2020-08-04 00:05:58 +0200

    add config3344

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/MainConfig3344.java
@@ -0,0 +1,18 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: 13720
+ * @date:
+ * @description:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class MainConfig3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainConfig3344.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author: 13720
+ * @date:
+ * @description:
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+        //2020-07-19T22:14:43.495+02:00[Europe/Paris]
+    }
+}
